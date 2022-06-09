a69b74c595cffe1489e083a403c85352ccfe85e0
Venkat <dvsuniq22@gmail.com>
2022-06-09 07:31:05 +0530

    First commit

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+import * as firebase from "firebase";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA3GZuQ-0J8jBAKxaCyM8ebMrpm4P5E4Bs",
+  authDomain: "ecommerce-fc6ab.firebaseapp.com",
+  projectId: "ecommerce-fc6ab",
+  storageBucket: "ecommerce-fc6ab.appspot.com",
+  messagingSenderId: "792375470410",
+  appId: "1:792375470410:web:485f1212c27cfd5224f9b3",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// export
+export const auth = firebase.auth();
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
